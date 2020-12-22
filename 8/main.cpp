d07413031205fa26ffbbb79edca68102f85dd90c
Nikita <nikita-kuznetsov-2018@list.ru>
2020-12-22 22:21:59 +0300

    8

--- 8/main.cpp
+++ 8/main.cpp
@@ -1,14 +1,57 @@
-//
-//  main.cpp
-//  8
-//
-//  Created by Никита Кузнецов on 22.12.2020.
-//
-
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+1 задание:
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main() {
+    int x;
+    cin >> x;
+    cout << x / 1024;
+}
+2 задание:
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << (a-b) / b;
+}
+3 задание:
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main() {
+    int a, b;
+    cin >> a >> b;
+    int X = (a - b) % b;
+    cout << X;
+}
+4 задание:
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main() {
+    int x;
+    int first, second;
+    cin >> x;
+    first = x / 10;
+    second = x % 10;
+    cout << second << first;
+}
+5 задание:
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main() {
+    int x;
+    int first, second, third;
+    cin >> x;
+    first = x / 100;
+    //cout << first;
+    second = x / 10;
+    second %= 10;
+    //cout << second;
+    third = x % 10;
+    //cout << third;
+    cout << second << third << first;
 }
